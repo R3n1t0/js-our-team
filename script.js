349c3a4c7ced2b9d143eb89ab686925cd7aebfc5
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-28 00:38:42 +0200

    quasi

--- script.js
+++ script.js
@@ -2,42 +2,37 @@ const container = document.querySelector(".team-container")
 const addMemberButton = document.getElementById("addMemberButton")
 
 
-const memberCards = [
-  /* {
-    name: "Wayne Barnett",
-    role: "Founder & CEO",
-    image: "img/wayne-barnett-founder-ceo.jpg"
-  }, */
+const members = [];
+
+
+// Ciclo For per inserire nel html la struttura della card prendendo quelli presenti nel members
+addMemberButton.addEventListener("click", memberdGenerator);{
+  for(let i in members){
+
+    container.innerHTML += 
+    `
+    <div class="team-card">
+      <div class="card-image">
+        <img src="${members[i].image}"/>
+      </div>
+      <div class="card-text">
+        <h3>${members[i].name}</h3>
+        <p>${members[i].role}</p>
+      </div>
+    </div>
+    `
+  }
+}
+
+//Funzione per generare le Card e li pusho nell members Array.
+function memberdGenerator(){
   
-]
+  const newMember = {
+    name: document.getElementById("name").value,
+    role: document.getElementById("role").value,
+    image: document.getElementById("image").value
+  }
 
-// Ciclo For per inserire nel html la struttura della card prendendo quelli presenti nel memberCards
-for(let i in memberCards){
-
-  container.innerHTML += 
-  `
-  <div class="team-card">
-    <div class="card-image">
-      <img src="img/wayne-barnett-founder-ceo.jpg"/>
-    </div>
-    <div class="card-text">
-      <h3></h3>
-      <p>Founder & CEO</p>
-    </div>
-  </div>
-  `
+  members.push(newMember);
+  console.log(members);
 }
-
-//Funzione per ricevere le info dagli Input.
-function getInputValue() {
-  let nameValue = document.getElementById("name").value;
-  let roleValue = document.getElementById("role").value;
-  let imageValue = document.getElementById("image").value;
-
-  memberCards.push({
-    name: nameValue,
-    role: roleValue,
-    image: imageValue
-  })
-}
-
