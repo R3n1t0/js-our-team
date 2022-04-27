0aed8cd0bd847b3a7dd78042cd31f6c45eaeb453
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-27 17:03:51 +0200

    continuando

---
+++ script.js
@@ -0,0 +1,32 @@
+const memberCards = [
+  {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    immagine: "img/wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    immagine: "img/wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    immagine: "img/wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    immagine: "img/wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    immagine: "img/wayne-barnett-founder-ceo.jpg"
+  }
+]
+
+
+function cardGenerator(params) {
+  
+}
