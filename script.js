0ce551cffeb355da1acc7b001454a8d1e0bcfd08
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-27 18:30:05 +0200

    input values

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
+const container = document.querySelector(".team-container")
+const addMemberButton = document.getElementById("addMemberButton")
+
+
 const memberCards = [
-  {
-    nome: "Wayne Barnett",
-    ruolo: "Founder & CEO",
-    immagine: "img/wayne-barnett-founder-ceo.jpg"
-  },
-  {
-    nome: "Wayne Barnett",
-    ruolo: "Founder & CEO",
-    immagine: "img/wayne-barnett-founder-ceo.jpg"
-  },
-  {
-    nome: "Wayne Barnett",
-    ruolo: "Founder & CEO",
-    immagine: "img/wayne-barnett-founder-ceo.jpg"
-  },
-  {
-    nome: "Wayne Barnett",
-    ruolo: "Founder & CEO",
-    immagine: "img/wayne-barnett-founder-ceo.jpg"
-  },
-  {
-    nome: "Wayne Barnett",
-    ruolo: "Founder & CEO",
-    immagine: "img/wayne-barnett-founder-ceo.jpg"
-  }
+  /* {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "img/wayne-barnett-founder-ceo.jpg"
+  }, */
+  
 ]
 
+// Ciclo For per inserire nel html la struttura della card prendendo quelli presenti nel memberCards
+for(let i in memberCards){
 
-function cardGenerator(params) {
-  
+  container.innerHTML += 
+  `
+  <div class="team-card">
+    <div class="card-image">
+      <img src="img/wayne-barnett-founder-ceo.jpg"/>
+    </div>
+    <div class="card-text">
+      <h3></h3>
+      <p>Founder & CEO</p>
+    </div>
+  </div>
+  `
 }
+
+//Funzione per ricevere le info dagli Input.
+function getInputValue() {
+  let nameValue = document.getElementById("name").value;
+  let roleValue = document.getElementById("role").value;
+  let imageValue = document.getElementById("image").value;
+
+  memberCards.push({
+    name: nameValue,
+    role: roleValue,
+    image: imageValue
+  })
+}
+
